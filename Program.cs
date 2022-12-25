1dd60c52d56473634ffe07c90b21c527cb89f84f
mennaemad1 <121379978+mennaemad1@users.noreply.github.com>
2022-12-25 12:55:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter the start number : ");
+        int start = int.Parse(Console.ReadLine());
+        Console.Write("Enter the end number : ");
+        int end = int.Parse(Console.ReadLine());
+        for (int i = start; i <= end; i++)
+        {
+            int count = 0;
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
